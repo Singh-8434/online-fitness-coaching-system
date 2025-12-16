fa241e4258b3a8b8626e8f1b93ce71bb199b1697
Singh-8434 <ujjwal.24scse1010598@galgotiasuniversity.ac.in>
2025-12-16 11:21:52 +0530

    Add User model class

---
+++ src/models/User.java
@@ -0,0 +1,81 @@
+package models;
+
+import java.time.LocalDateTime;
+
+/**
+ * User entity class representing system users
+ * Parent class for Members and Trainers
+ */
+public class User {
+    private int userId;
+    private String email;
+    private String passwordHash;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String userType; // MEMBER, TRAINER, ADMIN
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private boolean isActive;
+
+    // Constructor
+    public User(String email, String passwordHash, String firstName, 
+                String lastName, String phoneNumber, String userType) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.userType = userType;
+        this.createdDate = LocalDateTime.now();
+        this.updatedDate = LocalDateTime.now();
+        this.isActive = true;
+    }
+
+    // Getters and Setters
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public String getUserType() { return userType; }
+    public void setUserType(String userType) { this.userType = userType; }
+
+    public LocalDateTime getCreatedDate() { return createdDate; }
+    public void setCreatedDate(LocalDateTime createdDate) { this.createdDate = createdDate; }
+
+    public LocalDateTime getUpdatedDate() { return updatedDate; }
+    public void setUpdatedDate(LocalDateTime updatedDate) { this.updatedDate = updatedDate; }
+
+    public boolean isActive() { return isActive; }
+    public void setActive(boolean active) { isActive = active; }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userType='" + userType + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
